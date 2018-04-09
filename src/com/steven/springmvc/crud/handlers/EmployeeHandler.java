f2a7db4724560640082e036b635fe3e783adae50
zhiwenyan136 <1059408192@qq.com>
2018-04-09 15:59:39 +0800

    first commit

---
+++ src/com/steven/springmvc/crud/handlers/EmployeeHandler.java
@@ -0,0 +1,62 @@
+package com.steven.springmvc.crud.handlers;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.steven.springmvc.crud.dao.DepartmentDao;
+import com.steven.springmvc.crud.dao.EmployeeDao;
+import com.steven.springmvc.crud.entities.Employee;
+
+@Controller
+public class EmployeeHandler {
+	@Autowired
+	private EmployeeDao employeeDao;
+	@Autowired
+	private DepartmentDao departmentDao;
+
+	@RequestMapping("/emps")
+	public String list(Map<String, Object> map) {
+		map.put("employees", employeeDao.getAll());
+		return "list";
+	}
+
+	@RequestMapping(value = "/emp", method = RequestMethod.GET)
+	public String input(Map<String, Object> map) {
+		map.put("departments", departmentDao.getDepartments());
+		map.put("employee", new Employee());
+		return "input";
+	}
+
+	@RequestMapping(value = "/emp", method = RequestMethod.POST)
+	public String save(Employee employee) {
+		employeeDao.save(employee);
+		return "redirect:/emps";
+	}
+
+	@RequestMapping(value = "/emp/{id}", method = RequestMethod.DELETE)
+	public String delete(@PathVariable("id") Integer id) {
+		employeeDao.delete(id);
+		return "redirect:/emps";
+	}
+
+	@RequestMapping(value="/emp/{id}", method=RequestMethod.GET)
+	public String input(@PathVariable("id") Integer id, Map<String, Object> map){
+		map.put("employee", employeeDao.get(id));
+		map.put("departments", departmentDao.getDepartments());
+		return "input";
+	}
+	
+	@RequestMapping(value="/emp", method=RequestMethod.PUT)
+	public String update(Employee employee){
+		employeeDao.save(employee);
+		
+		return "redirect:/emps";
+	}
+	
+	
+}
